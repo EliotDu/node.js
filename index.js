eaf0f50dda53e79cf5810d76a28232bdc5fb421d
Eliot Du <ssvs1207@gmail.com>
2022-10-11 14:57:53 +0800

    1011node.js

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
+require('dotenv').config();
+const { request, response } = require('express');
 //引入express
 const express = require('express');
 //建立web server物件
 const app = express();
+
+
+//註冊樣版引擎
+app.set('view engine', 'ejs');
+
 //設定路由
 app.get('/',(req,res)=>{
-    res.send(`<span>找我幹嘛</span>`);
+    // res.send(`<span>找我幹嘛</span>`);
+    res.render('main',{name:'Eliot'});
 })
+
+app.get('/sales-json',(req,res)=>{
+    const sales = require(__dirname + '/data/sales');
+    console.log(sales);
+    res.render(`sales-json`,{sales})
+})
+
+app.get('/json-test',(req,res)=>{
+    res.json({name:'Eliot',age:25});
+})
+//取得queryString資料
+app.get('/try-qs',(req,res)=>{
+    res.json(req.query);
+})
+
+//取得POST資料
+const urlencodedParser = express.urlencoded({extends:false});
+app.post('/try-post',urlencodedParser,(req,res)=>{
+    res.json(req.body);
+})
+
+
+app.use(express.static('public'));
+//連接bootstrap路徑
+app.use(express.static('node_modules/bootstrap/dist'));
+
+
+app.use((req,res)=>{
+//    res.type('text/plain');//類型為純文字
+   res.status(404).render('404')
+})
+const port = process.env.SERVER_PORT || 3002;
 //server偵聽
-app.listen(3000,()=>{
-    console.log('server started');
+app.listen(port,()=>{
+    console.log(`server started port:${port}`);
 })
---
+++ readme.txt
@@ -0,0 +1,18 @@
+
+res.end()
+
+res.render()
+
+res.send()
+
+res.json()
+
+
+- - - - -
+RESTful API
+
+CRUD
+    C: POST
+    R: GET
+    U: PUT(PATCH)
+    D: DELETE
