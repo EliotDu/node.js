a8106bfc48958479ae95007c6a987056cb51526d
Eliot Du <ssvs1207@gmail.com>
2022-10-12 13:41:23 +0800

    1012node上傳多個檔案

--- index.js
+++ index.js
@@ -60,6 +60,10 @@ app.post('/try-upload',upload.single('avatar'), async (req,res)=>{
 //     }
  })
 
+app.post('/try-upload2',upload.array('photos'), async (req,res)=>{
+    res.json(req.files);
+ })
+
 app.use(express.static('public'));
 //連接bootstrap路徑
 app.use(express.static('node_modules/bootstrap/dist'));
